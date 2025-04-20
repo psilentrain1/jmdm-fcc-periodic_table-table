1a7ccf2850423beb84daf445d883b34d3fee835c
James Drake <github@jamesmdrakemedia.com>
2025-04-20 23:07:45 +0000

    Initial commit

---
+++ element.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [ ! "$1" ]; then
+  echo "Please provide an element as an argument."
+  exit 1
+fi
+
