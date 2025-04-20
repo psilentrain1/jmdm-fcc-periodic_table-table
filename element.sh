27c919a9981c15faf09ac42b4972516642f4e106
James Drake <github@jamesmdrakemedia.com>
2025-04-20 23:17:21 +0000

    feat: added SQL query

--- element.sh
+++ element.sh
@@ -4,5 +4,7 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 if [ ! "$1" ]; then
   echo "Please provide an element as an argument."
+else
+  QUERY="SELECT * FROM elements WHERE atomic_number = '$1' OR symbol = '$1' OR name = '$1';"
 fi
 
