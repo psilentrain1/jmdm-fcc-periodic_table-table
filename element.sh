6e03c419b0edd651a4df6b295725918bb56baf90
James Drake <github@jamesmdrakemedia.com>
2025-04-20 23:48:41 +0000

    feat: added fallback for not in database

--- element.sh
+++ element.sh
@@ -5,6 +5,11 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 if [ ! "$1" ]; then
   echo "Please provide an element as an argument."
 else
-  QUERY="SELECT * FROM elements WHERE atomic_number = '$1' OR symbol = '$1' OR name = '$1';"
+  ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = $(($1)) OR symbol = '$1' OR name = '$1';")
+  if [ -z $ATOMIC_NUMBER ]; then
+    echo "I could not find that element in the database."
+  else
+    echo "Continue"
+    fi
 fi
 
